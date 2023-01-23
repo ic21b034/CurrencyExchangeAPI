245e893fbdbe4f9361d686d63da72eb27ca2f184
wahid <ic21b034@technikum-wien.at>
2023-01-23 19:02:41 +0100

    implement ExchangeCurrency class

---
+++ src/main/java/at/fhtw/CurrencyExchangeAPI/ExchangeCurrency.java
@@ -0,0 +1,22 @@
+package at.fhtw.CurrencyExchangeAPI;
+
+public class ExchangeCurrency {
+
+    public double exchangeEuroToUSD(double euro){
+        double exc =   euro *  1.09;
+        return (double) Math.round(exc * 100) / 100;
+
+    }
+
+    public double exchangeEuroToGBP(double euro){
+        double exc = euro * 0.88;
+        return (double) Math.round(exc * 100) / 100;
+    }
+
+    public double exchangeEuroToJPY(double euro){
+        double exc = euro * 140.60;
+        return (double) Math.round(exc * 100) / 100;
+
+    }
+
+}
