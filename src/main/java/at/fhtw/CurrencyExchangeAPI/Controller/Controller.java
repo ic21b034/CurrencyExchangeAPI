65cea9f6be7c0c13165cb9c2716cd51a56cfe293
wahid <ic21b034@technikum-wien.at>
2023-01-23 19:01:15 +0100

    implement the Controller

--- src/main/java/at/fhtw/CurrencyExchangeAPI/Controller/Controller.java
+++ src/main/java/at/fhtw/CurrencyExchangeAPI/Controller/Controller.java
@@ -8,18 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
     ExchangeCurrency exc = new ExchangeCurrency();
+
     @GetMapping("/")
     String hello() {
         return"Hello";
     }
+
    @GetMapping("EuroToUSD/{euro}")
    double usd(@PathVariable double euro) {
        return exc.exchangeEuroToUSD(euro);
    }
+
     @GetMapping("EuroToGBP/{euro}")
     Double gpb(@PathVariable double euro) {
         return exc.exchangeEuroToGBP(euro);
     }
+
     @GetMapping("EuroToJPY/{euro}")
     double jpy(@PathVariable double euro) {
         return exc.exchangeEuroToJPY(euro);
